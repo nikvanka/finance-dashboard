dd0ed089b74df4b5dd362d24217742f8fb8acaba
unknown <vankanikhil@gmail.com>
2026-04-03 14:53:40 +0530

    feat: reorganize project structure and add advanced features

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import FinanceDashboard from './index.jsx'
+import FinanceDashboard from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
